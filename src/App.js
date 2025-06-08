fad11ba4775edf662b10c5fa36ccdbd91bf31f5f
David Murillo <david.josue.murillo23@gmail.com>
2025-06-07 20:21:46 -0500

    Agregue SafeAreaProvider al componente de la aplicación para mejorar el manejo del diseño

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import '../global.css';
 
 export default function App() {
   return (
-    <View className='m-auto'>
-      <Text className='font-bold text-lg'>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View className='m-auto'>
+        <StatusBar style="auto" />
+        <Text className='font-bold text-lg'>Open up App.js to start working on your app!</Text>
+      </View>
+    </SafeAreaProvider>
   );
 }
