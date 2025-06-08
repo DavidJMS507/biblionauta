7d746914d96eea545ded68c837ef9a066f67fa7f
David Murillo <david.josue.murillo23@gmail.com>
2025-06-07 23:03:17 -0500

    Eliminar el antiguo punto de entrada y reestructurar el diseño de la aplicación con nuevos componentes

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View, Button } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className='m-auto'>
+      <Text className='text-2xl font-bold'>Bienvenido a Biblionauta</Text>
+    </View>
+  );
+}
