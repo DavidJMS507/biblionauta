1df202cd2b1bf265a50edeb18a3581b7177bc5d4
David Murillo <david.josue.murillo23@gmail.com>
2025-06-24 15:09:19 -0500

    feat(icons): Agregue los iconos para los tabs

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,5 +2,21 @@ import Ionicons from '@expo/vector-icons/Ionicons'
 
 
 export const HomeIcon = (props) => (
-    <Ionicons name="library-outline" size={32} color="white" {...props} />
-);  
+  <Ionicons name="library-outline" size={32} color="white" {...props} />
+);
+
+export const UserIcon = (props) => {
+  <Ionicons name="person-outline" size={32} color="white" {...props} />
+}
+
+export const BookmarkIcon = (props) => {
+  <Ionicons name="bookmark-outline" size={32} color="white" {...props} />
+}
+
+export const TopBooksIcon = (props) => {
+  <Ionicons name="rocket-outline" size={32} color="white" {...props} />
+}
+
+export const SearchIcon = (props) => {
+  <Ionicons name="search-outline" size={32} color="white" {...props} />
+}
