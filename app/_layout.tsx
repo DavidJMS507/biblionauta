85c24b936231b6bcd538289baeec2d96997070c8
David Murillo <david.josue.murillo23@gmail.com>
2025-06-24 15:50:34 -0500

    feat(layout): Actualize el encabezado para usar BiblionautaLogo y personalizar el estilo del encabezado

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import { BiblionautaLogo } from "../src/components/LogoApp";
+import { StatusBar } from 'expo-status-bar';
 
 export default function Layout() {
   return (
     <SafeAreaProvider>
+      <StatusBar style="light" backgroundColor="#222" />
       <Stack screenOptions={{
         headerShown: true,
-        headerTitle: 'Index'
+        headerTitle: () => <BiblionautaLogo />,
+        headerStyle: { backgroundColor: "#222" },
+        headerShadowVisible: false
       }} />
     </SafeAreaProvider>
   )
