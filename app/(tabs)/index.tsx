16ca9b0063938125dc9a0cd7396e5527b8b2dad3
David Murillo <david.josue.murillo23@gmail.com>
2025-06-26 11:52:25 -0500

    fix(HomeScreen): Agregando un texto de prueba

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View className='w-full h-full bg-zinc-800'>
-      <Text className='text-2xl font-bold text-white'>Bienvenido a Biblionauta</Text>
+      <Text className='text-2xl font-bold text-white'>Bienvenido a Biblionauta - Texto de prueba</Text>
     </View>
   );
 }
